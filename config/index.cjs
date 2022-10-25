065665539deace6dbb1bd904a4db4ec895afc1b7
ZzzWww999 <116643648+ZzzWww999@users.noreply.github.com>
2022-10-26 02:35:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -57,8 +57,34 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id:'oBNf15l6k4WKGS1WO_flDbTOKydU',
+      id:
       
+ 
+    }
+  ],
+
+}
+
+module.exports = USER_CONFIG
+    
+    
+    
+    
+/* eslint-disable */
+
+/**
+ * 此项目配置为方便新人使用，已缩减至最简配置。
+ * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
+ * 自行添加属性，以支持更多个性化功能
+ */
+const USER_CONFIG = {
+
+  // 使用微信测试号：公众号APP_ID
+  APP_ID: 'wxe6c7f9579eaedc3f',
+
+  // 使用微信测试号：公众号APP_SECRET
+  APP_SECRET: '692e47e00aed8f0ce825af3ef3bee395',
+
   PROVINCE: '海南',
   CITY: '海口',
 
@@ -99,6 +125,13 @@ const USER_CONFIG = {
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: 'lnnlL0yT95Oh-kKXzEFChBe9bZNBCg9lwCfMT3W5S7A',
 
+  CALLBACK_USERS: [
+    {
+      name: '自己',
+      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id:
+      
+ 
     }
   ],
 
